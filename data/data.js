d793c169663878671051c315bea4e3a73e53dcc6
OlguinJv <olguin.j.vito@gmail.com>
2018-09-14 02:49:01 -0300

    first commit

---
+++ data/data.js
@@ -0,0 +1,33 @@
+var propiedad = {
+    titulo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ubicacion: "Juan Francisco Seguí, Palermo Chico, Palermo",
+    descripción: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    precio: "1.400.000",
+    m2: "380",
+    dormitorios: "3",
+    banos: "2",
+    cocheras: "2",
+    fotos: ["img/01.jpg", "img/02.jpg", "img/03.jpg"]
+}
+
+
+///////////////////////////////
+
+
+// var priceContainer = document.createElement("p");
+// var priceNumber = document.createTextNode("1400000");
+// priceContainer.appendChild(priceNumber);
+// document
+
+
+// function addElement () { 
+//     // crea un nuevo div 
+//     // y añade contenido 
+//     var newDiv = document.createElement("div"); 
+//     var newContent = document.createTextNode("Hola!¿Qué tal?"); 
+//     newDiv.appendChild(newContent); //añade texto al div creado. 
+  
+//     // añade el elemento creado y su contenido al DOM 
+//     var currentDiv = document.getElementById("div1"); 
+//     document.body.insertBefore(newDiv, currentDiv);
+//   }
---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+
+function activarFlechas(flecha, direccion) {
+    var flechaSeleccionada = document.getElementsByClassName(flecha);
+    flechaSeleccionada.addEventListener(click);
+}
