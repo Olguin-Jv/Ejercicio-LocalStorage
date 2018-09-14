449cc5f93282d78d25746c1045d736707efea420
OlguinJv <olguin.j.vito@gmail.com>
2018-09-14 06:34:03 -0300

    fast commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,32 @@
 
-function activarFlechas(flecha, direccion) {
-    var flechaSeleccionada = document.getElementsByClassName(flecha);
-    flechaSeleccionada.addEventListener(click);
+
+// funcion que almacena el like
+function toggleColor() {
+    var corazon = document.getElementById("toggle-color");
+    if (corazon.style.color == "red"){
+        corazon.style.color = "black";
+    } else {
+        corazon.style.color = "red";
+    }
+}
+
+var fotos = ["img/01.jpg", "img/02.jpg", "img/03.jpg"];
+var fotoActual = 0;
+
+function nextPhoto() {
+    var thumbnail = document.getElementById("fotos");
+    if (fotoActual == 2){
+        fotoActual = 0;
+        thumbnail.src = fotos[fotoactual];
+        fotoActual += 1;
+        console.log(thumbnail.src);
+    } else {
+        thumbnail.src = fotos[fotoactual];
+        fotoActual += 1;
+        console.log(fotoActual);
+    }
+}
+
+function previousPhoto(){
+    console.log("algo");
 }
