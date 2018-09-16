b8aaea88944ead3a7a96e6ecdc2ea620c0d10fa4
OlguinJv <olguin.j.vito@gmail.com>
2018-09-16 07:16:32 -0300

    fast commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,23 @@
+var corazon = document.getElementById("toggle-color");
 
+function setearLike() {
+    
+    if (localStorage.getItem("likeColor") == null){
+        console.log("no existe local storage");
+        localStorage.setItem("likeColor", "blue");
+        console.log(localStorage);
+    } else {
+        console.log("ya existe local storage");
+        console.log(localStorage);
+    }
 
-// funcion que cambia de color el corazon
-function toggleColor() {
     var corazon = document.getElementById("toggle-color");
+    corazon.style.color = localStorage.getItem("likeColor");
+};
+
+
+
+function toggleLike() {
     if (corazon.style.color == "red"){
         corazon.style.color = "black";
     } else {
@@ -10,18 +25,22 @@ function toggleColor() {
     }
 }
 
+window.onload = function(){
+    setearLike();
+}
+
 //funcion que calcula el precio por metro cuadrado
 function calcularPrecioMetroCuadrado(precio, superficie) {
     var precioPorM2 = parseInt(precio)/parseInt(superficie);
     var resultado = parseInt(precioPorM2); //le saco los decimales
     return resultado;
-}
+};
 
 //funcion que coloca los "." debidos a la cantidad numerica
 function valorNumerico(num) {
     var valor = parseInt(num);
     return valor.toLocaleString();
-}
+};
 
 function nextPhoto() {
     var fotoActual = document.getElementById("gallery");
@@ -36,7 +55,7 @@ function nextPhoto() {
     
     fotoNueva.src = propiedad.fotos[propiedad.fotosIndice];
     padre.replaceChild(fotoNueva, fotoActual);
-}
+};
 
 function previousPhoto(){
     var fotoActual = document.getElementById("gallery");
@@ -51,4 +70,4 @@ function previousPhoto(){
     
     fotoNueva.src = propiedad.fotos[propiedad.fotosIndice];
     padre.replaceChild(fotoNueva, fotoActual);
-}
+};
